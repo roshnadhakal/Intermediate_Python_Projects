a4e0fa0dcd28c656b34e88c885bfdb130c223159
roshnadhakal <146826039+roshnadhakal@users.noreply.github.com>
2024-09-09 13:26:25 +0545

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,9 +1,11 @@
-#Password Generator
+# Password Generator using random, string modules
 import random
 import string
 
 def generate_password(length):
+    # Defining characters to include in the password: letters, digits, and punctuation
     characters = string.ascii_letters + string.digits + string.punctuation
+    # Randomly choose characters for the length specified by the user
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
